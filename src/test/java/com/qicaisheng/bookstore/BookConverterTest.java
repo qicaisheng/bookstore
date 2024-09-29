f13511b87514f0368ce86bdae27227618adfcbb3
qicaisheng <qicaisheng@qq.com>
2024-09-29 10:57:33 +0800

    refactor: extract TestBookFactory

---
+++ src/test/java/com/qicaisheng/bookstore/BookConverterTest.java
@@ -0,0 +1,38 @@
+package com.qicaisheng.bookstore;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookConverterTest {
+
+    @Test
+    void testToEntity() {
+        BookPO bookPO = TestBookFactory.getBookPO();
+
+        Book book = BookConverter.toEntity(bookPO);
+
+        assertEquals("1", book.getId());
+        assertEquals("Author 1", book.getAuthor());
+        assertEquals(BookCategory.Business, book.getCategory());
+        assertEquals("Book Title", book.getTitle());
+        assertEquals(new BigDecimal("99.99"), book.getPrice().getValue());
+        assertEquals(Currency.CNY, book.getPrice().getCurrency());
+    }
+
+    @Test
+    void testToPO() {
+        Book book = TestBookFactory.getBook();
+
+        BookPO bookPO = BookConverter.toPO(book);
+
+        assertEquals("1", bookPO.getId());
+        assertEquals("Author 1", bookPO.getAuthor());
+        assertEquals(BookCategory.Business, bookPO.getCategory());
+        assertEquals("Book Title", bookPO.getTitle());
+        assertEquals(new BigDecimal("99.99"), bookPO.getPriceValue());
+        assertEquals(book.getPrice().getCurrency(), bookPO.getPriceCurrency());
+    }
+}
---
+++ src/test/java/com/qicaisheng/bookstore/TestBookFactory.java
@@ -0,0 +1,28 @@
+package com.qicaisheng.bookstore;
+
+import java.math.BigDecimal;
+
+public class TestBookFactory {
+    public static Book getBook() {
+        Book book = new Book();
+        book.setId("1");
+        book.setAuthor("Author 1");
+        book.setCategory(BookCategory.Business);
+        book.setTitle("Book Title");
+        book.setPrice(new Price(new BigDecimal("99.99"), Currency.CNY));
+
+        return book;
+    }
+
+    public static BookPO getBookPO() {
+        BookPO bookPO = new BookPO();
+        bookPO.setId("1");
+        bookPO.setAuthor("Author 1");
+        bookPO.setCategory(BookCategory.Business);
+        bookPO.setTitle("Book Title");
+        bookPO.setPriceValue(new BigDecimal("99.99"));
+        bookPO.setPriceCurrency(Currency.CNY);
+
+        return bookPO;
+    }
+}
